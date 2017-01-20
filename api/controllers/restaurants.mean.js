daa1c2dd52ea3aedd065baee16a9ef3a3512fd9a
Ashley Roach <asroach@cisco.com>
2017-01-20 13:32:23 +0100

    Update tutorial to include Mongo section

---
+++ api/controllers/restaurants.mean.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var Restaurants = require('./restaurants.model');
+
+module.exports = {
+    indexMean: indexMean,
+    createMean: createMean
+};
+
+function indexMean(req, res) {
+
+  Restaurants.find(function (err, contents) {
+    if(err) { return handleError(res, err); }
+    return res.status(200).json(contents);
+  });
+
+}
+
+function createMean(req, res) {
+
+  var newRestaurant = {
+    name: req.body.name,
+    address: req.body.address
+  }
+
+  Restaurants.create(newRestaurant, function(err, content) {
+    if(err) { return handleError(res, err); }
+    // console.log(content);
+    return res.status(201).json(content);
+  });
+
+}
