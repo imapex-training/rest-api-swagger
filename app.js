55d96b292c11404e40358f664a536bacd50dd4f9
Ashley Roach <asroach@cisco.com>
2017-01-31 13:08:55 -0700

    Revert "Update tutorial to include Mongo section"
    This reverts commit daa1c2dd52ea3aedd065baee16a9ef3a3512fd9a.

--- app.js
+++ app.js
@@ -2,28 +2,6 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
-
-// Use the Mongoose ORM for modeling your objects in MongoDB
-var mongoose = require('mongoose');
-
-// Declare the database connection
-var db = mongoose.connection;
-
-// If no MONGO environment variable exists, default to localhost
-if (!process.env.MONGO) {
-  process.env.MONGO = "mongodb://localhost:27017/restaurants";
-}
-
-// Connect to the database using the environment variable
-// process.env.MONGO
-mongoose.connect(process.env.MONGO);
-
-// Listen for events from Mongoose
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("DB Connected");
-});
-
 module.exports = app; // for testing
 
 var config = {
@@ -39,4 +17,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
 });
