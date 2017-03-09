2eab114ac1a44d6251aaa643c9768f0b369a2306
Ashley Roach <asroach@cisco.com>
2017-03-09 11:01:23 +1100

    Add docker cleanup

--- tools/workstation-reset.sh
+++ tools/workstation-reset.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-git fetch github
+git fetch origin
 git checkout master
-git reset --hard github/master
+git reset --hard origin/master
 git branch -d step8
+
+docker stop restaurants
+docker rm restaurants
