4e54375f0566d44d4ce922141afbbc4baa0878ef
Ashley Roach <asroach@cisco.com>
2017-03-09 11:15:03 +1100

    Tweaks for workstation reset

--- tools/workstation-reset.sh
+++ tools/workstation-reset.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
+echo "Removing rest-api-swagger directory"
 cd ~/workspace/ashley
 rm -rf rest-api-swagger
+
+echo "Cloning project"
 git clone https://github.com/CiscoDevNet/rest-api-swagger.git
 
+echo "Checkout step8"
+cd ~/workspace/ashley/rest-api-swagger
 git checkout -b step8 step8
 
+echo "Cleanup docker images"
 docker stop restaurants
 docker rm restaurants
